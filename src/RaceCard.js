c9d8dff533f79c75a0e3573dcd7b638e7ff8863f
synjan <95743791+synjan@users.noreply.github.com>
2024-03-06 12:50:43 +0100

    open/close

--- src/RaceCard.js
+++ src/RaceCard.js
@@ -1,4 +1,3 @@
-// RaceCard.js
 import React, { useState, useEffect } from 'react';
 import './RaceCard.css';
 import { calculateCountdown, formatSession } from './RaceUtils';
@@ -26,7 +25,6 @@ const countryCodes = {
   "United States": "US",
   "Qatar": "QA",
   "UAE": "AE",
-  
 };
 
 const RaceCard = ({
@@ -43,76 +41,71 @@ const RaceCard = ({
   coordinates,
   finished,
   top3,
-  isClosestFutureRace, // Assuming this prop is passed to indicate the closest future race
+  isClosestFutureRace,
 }) => {
   const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   const [raceCountdown, setRaceCountdown] = useState(calculateCountdown(`${date}T${time}`));
   const countryCode = countryCodes[country];
-  
+  const [isCollapsed, setIsCollapsed] = useState(true); // Start all cards in collapsed state
 
   useEffect(() => {
     const interval = setInterval(() => {
       setRaceCountdown(calculateCountdown(`${date}T${time}`));
     }, 1000);
-  
     return () => clearInterval(interval);
   }, [date, time]);
 
-  const displaySessionInfo = (session, sessionName) => {
-    if (!session || !session.date || !session.time) return `${sessionName} is not scheduled.`;
-    const sessionDateTime = new Date(`${session.date}T${session.time}`);
-    const now = new Date();
-  
-    if (sessionDateTime <= now) {
-      return `${sessionName} has already started or finished.`;
-    } else {
-      const localTime = formatSession(session, 'UTC');
-      const userTime = formatSession(session, userTimeZone);
-      const countdown = calculateCountdown(sessionDateTime.toISOString());
-      return `${sessionName} starts in ${countdown}. It starts at ${userTime} your time (${localTime}).`;
-    }
-  };
+  const toggleCollapse = () => setIsCollapsed(!isCollapsed);
 
   const mapImageUrl = `https://api.mapbox.com/styles/v1/superjan/clt73t4aa00yi01qua3sbbo1t/static/${coordinates.long},${coordinates.lat},10/500x300@2x?access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`;
   const cardClasses = `race-card ${isClosestFutureRace ? 'closest-future-race' : ''}`;
 
   return (
-    <div className={cardClasses} style={{ backgroundImage: `url(${mapImageUrl})` }}>
+    <div className={cardClasses} style={{ backgroundImage: `url(${mapImageUrl})` }} onClick={toggleCollapse}>
       <div className="race-card-overlay"></div>
       <div className="race-card-content">
-        
         <h3 className="race-name">{raceName}
-        <div className="flag-container">
-          <img src={`https://flagsapi.com/${countryCode}/flat/64.png`} alt={`${country} flag`} style={{ width: '64px', height: '64px' }} />
-        </div>
+          <div className="flag-container">
+            <img src={`https://flagsapi.com/${countryCode}/flat/64.png`} alt={`${country} flag`} style={{ width: '64px', height: '64px' }} />
+          </div>
         </h3>
-        
-        <p className="circuit-name">{circuitName} - {locality}, {country}</p>
-        <p className="race-countdown">{finished ? "Race Finished" : raceCountdown}</p>
-        
-        <p className="date">Race Date: {date}</p>
-        <p className="time">Local Time: {formatSession({date, time}, 'UTC')}</p>
-        <p className="user-time">Your Time: {formatSession({date, time}, userTimeZone)}</p>
-        
-        {
-  finished && top3 ? (
-    <>
-      <p className="finished-message">This race has concluded.</p>
-      <div className="top-drivers">
-        <h4>Top 3 Drivers:</h4>
-        <ul>
-          {top3.map((driver, index) => (
-            <li key={index}>{driver.position}. {driver.driver} - {driver.time}</li>
-          ))}
-        </ul>
-      </div>
-    </>
-  ) : (
+        {isCollapsed ? (
+          // Collapsed view
+          <p className="race-summary">
+            {finished ? 
+              `Winner: ${top3 ? top3[0].driver : 'N/A'}` : // Display winner's name if race is finished
+              `Starts in: ${raceCountdown}` // Display countdown if race is upcoming
+            }
+          </p>
+        ) : (
+          // Expanded view
           <>
-            <p className="quali">{displaySessionInfo(qualifying, "Qualifying")}</p>
-            <p className='practice'>{displaySessionInfo(firstPractice, "First Practice")}</p>
-            <p className='practice'>{displaySessionInfo(secondPractice, "Second Practice")}</p>
-            <p className='practice'>{displaySessionInfo(thirdPractice, "Third Practice")}</p>
+            <p className="circuit-name">{circuitName} - {locality}, {country}</p>
+            <p className="race-countdown">{finished ? "Race Finished" : raceCountdown}</p>
+            <p className="date">Race Date: {date}</p>
+            <p className="time">Local Time: {formatSession({date, time}, 'UTC')}</p>
+            <p className="user-time">Your Time: {formatSession({date, time}, userTimeZone)}</p>
+            {finished && top3 && (
+              <>
+                <p className="finished-message">This race has concluded.</p>
+                <div className="top-drivers">
+                  <h4>Top 3 Drivers:</h4>
+                  <ul>
+                    {top3.map((driver, index) => (
+                      <li key={index}>{driver.position}. {driver.driver} - {driver.time}</li>
+                    ))}
+                  </ul>
+                </div>
+              </>
+            )}
+            {!finished && (
+              <>
+                <p className="quali">{qualifying && `Qualifying starts at ${formatSession(qualifying, userTimeZone)}`}</p>
+                <p className='practice'>{firstPractice && `First Practice starts at ${formatSession(firstPractice, userTimeZone)}`}</p>
+                <p className='practice'>{secondPractice && `Second Practice starts at ${formatSession(secondPractice, userTimeZone)}`}</p>
+                <p className='practice'>{thirdPractice && `Third Practice starts at ${formatSession(thirdPractice, userTimeZone)}`}</p>
+              </>
+            )}
           </>
         )}
       </div>
