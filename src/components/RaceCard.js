3ccf0b946017b7ac333d2d5ef4bdd1535b05544d
synjan <95743791+synjan@users.noreply.github.com>
2024-04-03 15:44:17 +0200

    tracks

--- src/components/RaceCard.js
+++ src/components/RaceCard.js
@@ -7,6 +7,32 @@ const TIME_ZONE = "Europe/Oslo";
 const RACE_DURATION_HRS = 2 * 60 * 60 * 1000;
 const QUALIFYING_SPRINT_DURATION_HRS = 1 * 60 * 60 * 1000;
 
+const circuitImages = {
+  bahrain: require("../images/bahrain.png"),
+  jeddah: require("../images/jeddah.png"),
+  albert_park: require("../images/albert_park.png"),
+  shanghai: require("../images/shanghai.png"),
+  miami: require("../images/miami.png"),
+  imola: require("../images/imola.png"),
+  monaco: require("../images/monaco.png"),
+  catalunya: require("../images/catalunya.png"),
+  villeneuve: require("../images/villeneuve.png"),
+  silverstone: require("../images/silverstone.png"),
+  red_bull_ring: require("../images/red_bull_ring.png"),
+  hungaroring: require("../images/hungaroring.png"),
+  spa: require("../images/spa.png"),
+  zandvoort: require("../images/zandvoort.png"),
+  monza: require("../images/monza.png"),
+  marina_bay: require("../images/marina_bay.png"),
+  suzuka: require("../images/suzuka.png"),
+  americas: require("../images/americas.png"),
+  rodriguez: require("../images/rodriguez.png"),
+  interlagos: require("../images/interlagos.png"),
+  vegas: require("../images/vegas.png"),
+  losail: require("../images/losail.png"),
+  yas_marina: require("../images/yas_marina.png"),
+};
+
 const RaceCard = ({ race, onClick }) => {
   const { raceName, Circuit, date, time, Qualifying, Sprint } = race;
   const raceDateTime = new Date(`${date}T${time}`);
@@ -54,71 +80,81 @@ const RaceCard = ({ race, onClick }) => {
     timeZone: TIME_ZONE,
   };
 
+  const circuitImage = circuitImages[Circuit.circuitId];
+
   return (
     <div
       className={`race-card ${raceStatus.toLowerCase()}`}
       onClick={handleClick}
     >
-      <h2>{raceName}</h2>
-      <p>{Circuit.circuitName}</p>
-      <p>
-        Qualifying: {format(qualifyingDateTime, "EEEE, MMMM d, yyyy - h:mm a")}
-      </p>
-      {isUpcoming && (
-        <AddToCalendarButton
-          {...calendarButtonProps}
-          name={`Qualifying: ${raceName}`}
-          description={`Qualifying for ${raceName} at ${Circuit.circuitName}`}
-          startDate={format(qualifyingDateTime, "yyyy-MM-dd")}
-          startTime={format(qualifyingDateTime, "HH:mm")}
-          endTime={format(
-            new Date(
-              qualifyingDateTime.getTime() + QUALIFYING_SPRINT_DURATION_HRS,
-            ),
-            "HH:mm",
-          )}
-          location={Circuit.circuitName}
-        />
-      )}
-      {Sprint && (
-        <>
-          <p>Sprint: {format(sprintDateTime, "EEEE, MMMM d, yyyy - h:mm a")}</p>
-          {isUpcoming && (
-            <AddToCalendarButton
-              {...calendarButtonProps}
-              name={`Sprint: ${raceName}`}
-              description={`Sprint for ${raceName} at ${Circuit.circuitName}`}
-              startDate={format(sprintDateTime, "yyyy-MM-dd")}
-              startTime={format(sprintDateTime, "HH:mm")}
-              endTime={format(
-                new Date(
-                  sprintDateTime.getTime() + QUALIFYING_SPRINT_DURATION_HRS,
-                ),
-                "HH:mm",
-              )}
-              location={Circuit.circuitName}
-            />
-          )}
-        </>
-      )}
-      <p>Race: {format(raceDateTime, "EEEE, MMMM d, yyyy - h:mm a")}</p>
-      <p>{countdownText}</p>
-      {isUpcoming && (
-        <div className="calendar-section">
+      <div className="race-card-content">
+        <h2>{raceName}</h2>
+        <p>{Circuit.circuitName}</p>
+        <p>
+          Qualifying:{" "}
+          {format(qualifyingDateTime, "EEEE, MMMM d, yyyy - h:mm a")}
+        </p>
+        {isUpcoming && (
           <AddToCalendarButton
             {...calendarButtonProps}
-            name={raceName}
-            description={`${raceName} at ${Circuit.circuitName}`}
-            startDate={format(raceDateTime, "yyyy-MM-dd")}
-            startTime={format(raceDateTime, "HH:mm")}
+            name={`Qualifying: ${raceName}`}
+            description={`Qualifying for ${raceName} at ${Circuit.circuitName}`}
+            startDate={format(qualifyingDateTime, "yyyy-MM-dd")}
+            startTime={format(qualifyingDateTime, "HH:mm")}
             endTime={format(
-              new Date(raceDateTime.getTime() + RACE_DURATION_HRS),
+              new Date(
+                qualifyingDateTime.getTime() + QUALIFYING_SPRINT_DURATION_HRS,
+              ),
               "HH:mm",
             )}
             location={Circuit.circuitName}
           />
-        </div>
-      )}
+        )}
+        {Sprint && (
+          <>
+            <p>
+              Sprint: {format(sprintDateTime, "EEEE, MMMM d, yyyy - h:mm a")}
+            </p>
+            {isUpcoming && (
+              <AddToCalendarButton
+                {...calendarButtonProps}
+                name={`Sprint: ${raceName}`}
+                description={`Sprint for ${raceName} at ${Circuit.circuitName}`}
+                startDate={format(sprintDateTime, "yyyy-MM-dd")}
+                startTime={format(sprintDateTime, "HH:mm")}
+                endTime={format(
+                  new Date(
+                    sprintDateTime.getTime() + QUALIFYING_SPRINT_DURATION_HRS,
+                  ),
+                  "HH:mm",
+                )}
+                location={Circuit.circuitName}
+              />
+            )}
+          </>
+        )}
+        <p>Race: {format(raceDateTime, "EEEE, MMMM d, yyyy - h:mm a")}</p>
+        <p>{countdownText}</p>
+        {isUpcoming && (
+          <div className="calendar-section">
+            <AddToCalendarButton
+              {...calendarButtonProps}
+              name={raceName}
+              description={`${raceName} at ${Circuit.circuitName}`}
+              startDate={format(raceDateTime, "yyyy-MM-dd")}
+              startTime={format(raceDateTime, "HH:mm")}
+              endTime={format(
+                new Date(raceDateTime.getTime() + RACE_DURATION_HRS),
+                "HH:mm",
+              )}
+              location={Circuit.circuitName}
+            />
+          </div>
+        )}
+      </div>
+      <div className="race-card-image">
+        <img src={circuitImage} alt={Circuit.circuitName} />
+      </div>
     </div>
   );
 };
